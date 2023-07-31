39c64a0dbf35d1a19a588f703e660daedd44305e
Eluan96 <eluan.ea@gmail.com>
2023-07-31 21:16:18 +0100

    reset password

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
-import User  from '../model/user'
+import User, { IUSER }  from '../model/user'
 import {v4} from "uuid";
-import { hashedPassword } from "./utils/auth";
+import { generateToken, hashedPassword, resetPasswordOTP, verifyToken } from "./utils/auth";
 import { genAccount} from "./utils/auth";
 import { generateOTP } from './utils/auth'
 import {emailHtml, sendmail} from './utils/notifications';
+import nodemailer from 'nodemailer'
 import dotenv from 'dotenv';
 dotenv.config()
 // import {database} from '../config/index'
@@ -71,3 +72,92 @@ export const userLogin = async(req: Request, res: Response, next: NextFunction)=
 export const forgotPassword = async(req: Request, res: Response, next: NextFunction)=>{
     res.json("Recover password")
 }
+
+
+export const reqResetPasswordOTP = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: process.env.DEV_GMAIL_USER,
+                pass: process.env.DEV_GMAIL_PASSWORD
+            }
+        });
+    
+        const { email } = req.body;
+    
+        // Find user based on email
+        const user = await User.findOne({ where: { email } });
+    
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+    
+        // Generate a random four-digit OTP
+        const otp = resetPasswordOTP(); // You need to implement this function
+    
+        // Generate token for the user (assuming generateToken is asynchronous)
+        const token = await generateToken(user.get()); // Get plain object of the user from the query result
+    
+        // Compose mail
+        const mailOptions = {
+            from: process.env.DEV_GMAIL_USER,
+            to: user.get().email,
+            subject: 'Password Reset OTP',
+            text: `Your OTP for password reset is: ${otp}`
+        };
+    
+        // Send email
+        transporter.sendMail(mailOptions, (error) => {
+            if (error) {
+                console.log('Error sending email:', error);
+                return res.status(500).json({ message: 'Error sending OTP email' });
+            }
+    
+            return res.status(200).json({ message: 'OTP sent successfully' });
+        });
+    
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid User' });
+    }
+};
+
+
+export const verifyResetPasswordOTP = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, otp, newPassword } = req.body;
+
+    try {
+        // Find user based on email
+        const user = await User.findOne({ where: { email } }) as unknown as IUSER
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Verify OTP
+        const decoded = verifyToken(otp);
+
+        if (!decoded || decoded?.OTP !== otp) {
+            return res.status(401).json({ message: 'Invalid OTP' });
+        }
+
+        if (Date.now() / 1000 > decoded.exp) {
+            return res.status(401).json({ message: 'OTP has expired' });
+        }
+
+        // Check password
+        if (newPassword === user.password) {
+            return res.status(400).json({ message: 'NewPassword cannot be the same as OldPassword' });
+        }
+
+        // Update the user's password using the Sequelize update method
+        await User.update(
+            { password: newPassword },
+            { where: { id: user.id } }
+        );
+
+        return res.status(200).json({ message: 'Password reset successful' });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+};
--- src/controllers/utils/auth.ts
+++ src/controllers/utils/auth.ts
@@ -1,7 +1,10 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import otpGenerator from "otp-generator";
 import { OTP_LENGTH, OTP_CONFIG } from "./notifications";
+import dotenv  from "dotenv";
+dotenv.config()
+
 
 export const hashedPassword = async (password: string) => {
   const saltRounds = 10;
@@ -20,3 +23,40 @@ export const generateOTP = () => {
   const OTP = otpGenerator.generate(OTP_LENGTH, OTP_CONFIG);
   return OTP;
 }
+
+export const resetPasswordOTP = () => {
+  const optExpiryMinutes = 6
+  const otp = Math.floor(1000 + Math.random() * 9000).toString()
+  const expirationTime = new Date()
+  expirationTime.setMinutes(expirationTime.getMinutes() + optExpiryMinutes)
+  const response = {
+    otp,
+    expiresAt: expirationTime.toISOString()
+  }
+
+  return response
+}
+
+export const generateToken = (user:any) => {
+  const payload = {
+      userId: user._id,
+      otp: user.otp,
+      exp: Math.floor(Date.now() / 1000) + 3600
+      // Add any other relevant data to the payload if needed
+    };
+
+  const token = jwt.sign(payload, `${process.env.APP_SECRET}`)
+  return token
+}
+
+
+
+export const verifyToken = (token: string) => {
+  try {
+    const decoded = jwt.verify(token, `${process.env.APP_SECRET}`) as object;
+    return decoded as { userId: number; OTP: string; exp: number };
+  } catch (err) {
+    console.error('Token verification failed:', err);
+    return null;
+  }
+};
