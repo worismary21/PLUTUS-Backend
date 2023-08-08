16d48480437240927403bb0a252201a102b428ae
Afinjuomo <afinjuomooluwatunde17@gmail.com>
2023-08-08 21:01:18 +0100

    Beneficiary

--- src/routes/beneficiary.routes.ts
+++ src/routes/beneficiary.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createBeneficiaries, deleteBeneficiary, getBeneficiaries } from "../controllers/beneficiary";
+import { auth } from '../middleware/auth';
 
 const router = express.Router();
 
-router.get('/:id', getBeneficiaries);
-router.post('/create', createBeneficiaries);
-router.delete('/:id', deleteBeneficiary);
+router.get('/get', auth, getBeneficiaries);
+router.post('/create', auth, createBeneficiaries);
+router.delete('/delete', auth, deleteBeneficiary);
 
 
 
