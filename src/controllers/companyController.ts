a3795b22a52e643eeefbd5df4e3ba7d251122613
Eluan96 <eluan.ea@gmail.com>
2023-08-16 00:13:29 +0100

    done

--- src/controllers/companyController.ts
+++ src/controllers/companyController.ts
@@ -104,7 +104,7 @@ export const createCompany = async (
 
   export const getAllCompanies = async (req: Request, res: Response, next: NextFunction) => {
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 5;
 
     const {offset, limit: paginationLimit} = getPagination({page, limit})
 
--- src/routes/company.route.ts
+++ src/routes/company.route.ts
@@ -7,8 +7,8 @@ import { isAdmin } from '../controllers/utils/auth';
 const router = express.Router();
 
 router.post('/create', isAdmin, createCompany);
-router.delete('/delete/id', deleteCompany)
-router.get('/get-companies', getAllCompanies)
+router.delete('/delete/:id', isAdmin, deleteCompany)
+router.get('/get-companies', isAdmin, getAllCompanies)
 router.post('/login', loginCompany);
 
 
